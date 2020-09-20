85f6b65d70a523a04b961d78ef35e8902989b15b
João Rodrigues <joao.rodrigues@inova.business>
2020-09-20 12:22:09 +0100

    CORS enabled

--- backend/app.ts
+++ backend/app.ts
@@ -1,25 +1,14 @@
 import { ErrorMiddleware } from './middlewares/errorMiddleware.ts';
-import { Application, HttpMethod, DefaultSkipper, CORSConfig } from 'https://deno.land/x/abc@v1/mod.ts'
+import { Application } from 'https://deno.land/x/abc@v1/mod.ts'
+import { abcCors } from 'https://deno.land/x/cors/abcCors.ts'
 import { getIngredients } from './controller/ingredientsController.ts'
 import "https://deno.land/x/denv/mod.ts";
-const DefaultCORSConfig: CORSConfig = {
-    skipper: DefaultSkipper,
-    allowOrigins: ["*"],
-    allowMethods: [
-      HttpMethod.Delete,
-      HttpMethod.Get,
-      HttpMethod.Head,
-      HttpMethod.Patch,
-      HttpMethod.Post,
-      HttpMethod.Put
-    ]
-  };
 
 const PORT = 7700
 
 const app = new Application()
 
-app.use(cors(DefaultCORSConfig))
+app.use(abcCors())
 
 app.get('/ingredients', getIngredients)
 
